e60834a6f18e5386f9abe5741214db82c2c4d8f0
Lou BEGE <lou.bege@kaizen-solutions.net>
2023-04-29 11:14:01 +0200

    Fix Transparent

--- l2j3/objects/Thing.py
+++ l2j3/objects/Thing.py
@@ -5,7 +5,7 @@ class Thing:
         self.rect = rect
         self.speed = speed
         self.DEFAULT_IMAGE_SIZE = (rect.width, rect.height)
-        self.image = pygame.image.load(image).convert()
+        self.image = pygame.image.load(image).convert_alpha()
         self.image = pygame.transform.scale(self.image, self.DEFAULT_IMAGE_SIZE)
     
     def Move(self, x, y):
@@ -15,5 +15,5 @@ class Thing:
         self.rect.width = width
         self.rect.height = height
         self.DEFAULT_IMAGE_SIZE = (width, height)
-        self.image = pygame.image.load(image).convert()
+        self.image = pygame.image.load(image).convert_alpha()
         self.image = pygame.transform.scale(self.image, self.DEFAULT_IMAGE_SIZE)
