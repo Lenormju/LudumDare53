07fcee43d9e27d37689bf2ed6ebe1aa73de45a08
Julien LENORMAND <julien.lenormand@kaizen-solutions.net>
2023-04-29 12:04:51 +0200

    quit withe scape

--- l2j3/screens/GoodEndingGood.py
+++ l2j3/screens/GoodEndingGood.py
@@ -10,7 +10,7 @@ comic_sans_ms_small = pygame.font.SysFont('Comic Sans MS', 12)
 
 def render(screen, events, keys):
     for event in events:
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             GAME_INFO.NEXT_GAME_SCREEN = GameScreen.QUIT
 
     screen.fill(black_color)  # === draw _AFTER_ this line ===
@@ -19,4 +19,4 @@ def render(screen, events, keys):
     screen.blit(comic_sans_ms.render("your score was :", False, ludum_dark_orange), (0, 50))
     screen.blit(comic_sans_ms.render("TODO", False, granny_color), (240, 50))
     screen.blit(comic_sans_ms_small.render("PS: have you tried not to catch the babies ?", False, evil_red), (0, 400))
-    screen.blit(comic_sans_ms.render("press [space] to quit", False, yellow_color), (0, 500))
+    screen.blit(comic_sans_ms.render("press [escape] to quit", False, yellow_color), (0, 500))
